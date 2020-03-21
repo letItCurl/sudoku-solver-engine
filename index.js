7b069c9f9acb603de224775d63473a0d45304b56
rollpz <rolandlopez.developer@gmail.com>
2020-03-21 10:08:10 +0100

    Working on sudoku object

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-const {displaySudoku} = require('./src/display')
-const {sudoku} = require('./src/sudoku')
-displaySudoku(sudoku)
-console.log(sudoku)
+
+
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,14 +1,63 @@
-const row1 = [6,0,7,9,0,0,2,0,3]
-const row2 = [9,0,3,4,2,0,8,6,0]
-const row3 = [0,0,0,0,8,3,0,0,1]
-const row4 = [5,3,0,0,6,0,9,0,2]
-const row5 = [0,0,0,0,0,0,0,3,7]
-const row6 = [4,0,0,1,3,2,5,0,0]
-const row7 = [0,4,0,0,7,0,6,0,9]
-const row8 = [7,2,0,0,0,0,0,0,0]
-const row9 = [8,9,1,2,5,0,0,7,0]
-const sudoku = [] 
-sudoku.push(row1,row2,row3,row4,row5,row6,row7,row8,row9)
+const data = require('./sudokuData')
+const colors = require('colors')
 
-exports.sudoku = sudoku
+function Sudoku(sudoku){
+    
+    this.sudoku = sudoku
 
+    this.displaySudoku = function(){
+    console.log("All sudoku".red.bold)
+    console.log("+-----------+".red.bold)
+    for (let x = 0; x < 9; x++) {
+        var str = this.sudoku[x].join().replace(/,/g,"")
+        str = "|".red.bold+str.slice(0,3)+"|".red.bold+str.slice(3,6)+"|".red.bold+str.slice(-3)+"|".red.bold
+        console.log(str)
+        if (x === 2){
+            console.log("|-----------|".red.bold)  
+        }
+        if(x === 5){
+            console.log("|-----------|".red.bold)
+        }
+    }
+    console.log("+-----------+".red.bold)
+    }
+
+    this.displayRow = function(x){
+        console.log(`row@${x+1}:`.red.bold)
+        console.log(`${this.sudoku[x].join().replace(/,/g," ").yellow}`)
+    }
+    this.displayCol = function(y){
+        console.log(`col@${y+1}: `.red.bold)
+        for (let x = 0; x < 9; x++) {
+            if(x===0){
+                console.log(`${this.sudoku[x][y]}`.yellow)
+            }else{
+                console.log(`${this.sudoku[x][y]}`.yellow)
+            }
+        }
+    }
+    this.displayCross = function(x,y){
+        var space = ""
+        console.log(`cross@${x},${y}: `.red.bold)
+        for(let i = 0; i < y; i++){
+            space = `${space}  `
+        }
+        for (let scanX = 0; scanX < 9; scanX++) {
+            if(scanX===x){
+                console.log(this.sudoku[x].join().replace(/,/g," ").yellow)
+            }else{
+                console.log(space+`${this.sudoku[scanX][y]}`.yellow)
+            }
+        }
+    }
+    this.getRow = function(x){
+        return this.sudoku[x]
+    }
+    this.getCol = function(y){
+        var col = []
+        for (let x = 0; x < 9; x++) {
+            col.push(this.sudoku[x][y])
+        }
+        return col
+    }
+}
