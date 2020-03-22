206794b8d338797fe5c814021677030c86a3637c
rollpz <rolandlopez.developer@gmail.com>
2020-03-22 13:20:41 +0100

    Modularisation done

--- src/engine.js
+++ src/engine.js
@@ -1,11 +1,10 @@
 const { checkCol, checkRow, checkArea } = require('./rules');
-
-
-
-
+const ToolBox = require('./toolBox')
 
 const {grid} = require('./sudokuData')
 const Sudoku = require('./sudoku')
+
+const myTooBox = new ToolBox()
 
 //Sudoku.prototype.checkRow = checkRow
 //Sudoku.prototype.checkCol = checkCol
@@ -14,28 +13,37 @@ const Sudoku = require('./sudoku')
 //mySudok.tools = getRowFromToolBox.bind(mySudok); for mod one object
 
 const mySudok = new Sudoku(grid)
-const copiedSudoku = copy(mySudok.grid)
+
 var n = 9
 var centers = [[1,1],[4,1],[7,1],[1,4],[4,4],[7,4],[1,7],[4,7],[7,7]]
 
-copiedSudoku.displayGrid("initial number: "+n)
-hideImpossibilities(copiedSudoku, n)
+const solveN = function(originalSudoku, n){
+    var count = 0
+    const refSudoku = myTooBox.copy(originalSudoku.grid)
+    refSudoku.displayGrid("initial number: "+n)
+    myTooBox.hideImpossibilities(refSudoku, n)
+    refSudoku.displayGrid("hided number: "+n)
+    
+    centers.forEach(center => { 
+        if(myTooBox.canWePlace(refSudoku.getArea(center[0],center[1]))){
+            console.log("we can replace here!".green+center)
+            console.log(`--- START REPLACEMENT FOR:${n} @center:${center} ---`.magenta)
+            console.log("Here is the area:")
+            console.log(refSudoku.getArea(center[0],center[1]))
+            myTooBox.scanAreaAndReplaceZero(refSudoku ,originalSudoku, center, n)
+            console.log(`--- END REPLACEMENT FOR:${n} @center:${center} ---`.magenta)
+            count ++;
+    
+        }else{
+            console.log("No replacement possible @area: "+center)
+        }
+    })
+    
+    originalSudoku.displayGrid("final mod")
+    return (count === 0) ? true : false
+}
 
-copiedSudoku.displayGrid("hided number: "+n)
+solveN(mySudok, n) ? console.log("on ne peut pas aller plus loin !".green) : console.log("il y a encore des étapes...".red)
+solveN(mySudok, n) ? console.log("on ne peut pas aller plus loin !".green) : console.log("il y a encore des étapes...".red)
 
-centers.forEach(center => { 
-    if(canWePlace(copiedSudoku.getArea(center[0],center[1]))){
-        console.log("we can replace here!".green+center)
-        console.log(`--- START REPLACEMENT FOR:${n} @center:${center} ---`.magenta)
-        console.log("Here is the area:")
-        console.log(copiedSudoku.getArea(center[0],center[1]))
-        scanAreaAndReplaceZero(copiedSudoku ,mySudok, center, n)
-        console.log(`--- END REPLACEMENT FOR:${n} @center:${center} ---`.magenta)
 
-    }else{
-        console.log("No replacement possible @area: "+center)
-        //console.log(copiedSudoku.getArea(center[0],center[1]))
-    }
-})
-
-mySudok.displayGrid("final mod")
--- src/toolBox.js
+++ src/toolBox.js
@@ -2,39 +2,59 @@ const _ = require('underscore')
 const {grid} = require('./sudokuData')
 const Sudoku = require('./sudoku')
 
-const copy = function(data) {
-    return new Sudoku (JSON.parse(JSON.stringify(data)))
-}
-const returnSudoku = function(obj) {
-    return (JSON.parse(JSON.stringify(obj.grid)))
-}
-const replaceRow = function(obj, y) {
-    obj.grid[y] = ["x","x","x","x","x","x","x","x"]
-}
-const replaceCol = function(obj, x) {
-    for(let scanY = 0; scanY < 9 ; scanY++){
-        obj.grid[scanY][x] = "x"
+class ToolBox{
+    constructor(){
+
     }
-}
-
-const canWePlace = function(area){
-    var str = area.join().replace(/,|x|1|6|2|3|4|5|6|7|8|9/g,"")
-    if(str==="0"){return true}
-}
-
-const scanAreaAndReplaceZero = function(ref, obj, center, val){
-    for(let i = center[0]-1; i < center[0]+2; i++){
-        for(let j = center[1]-1; j < center[1]+2; j++){
-            if(ref.grid[j][i]===0){
-                obj.grid[j][i]=val
-                console.log("REPLACEMENT".green+" @: "+i+" "+j)
-            }else{
-                console.log("NOT REPLACED".yellow + ref.grid[j][i])
+    copy = function(data) {
+        return new Sudoku (JSON.parse(JSON.stringify(data)))
+    }
+    returnSudoku = function(obj) {
+        return (JSON.parse(JSON.stringify(obj.grid)))
+    }
+    replaceRow = function(obj, y) {
+        obj.grid[y] = ["x","x","x","x","x","x","x","x"]
+    }
+    replaceCol = function(obj, x) {
+        for(let scanY = 0; scanY < 9 ; scanY++){
+            obj.grid[scanY][x] = "x"
+        }
+    }
+    
+    canWePlace = function(area){
+        var str = area.join().replace(/,|x|1|6|2|3|4|5|6|7|8|9/g,"")
+        if(str==="0"){return true}
+    }
+    
+    scanAreaAndReplaceZero = function(ref, obj, center, val){
+        for(let i = center[0]-1; i < center[0]+2; i++){
+            for(let j = center[1]-1; j < center[1]+2; j++){
+                if(ref.grid[j][i]===0){
+                    obj.grid[j][i]=val
+                    console.log("REPLACEMENT".green+" @: "+i+" "+j)
+                }else{
+                    console.log("NOT REPLACED".yellow + ref.grid[j][i])
+                }
             }
         }
     }
+    hideImpossibilities = function(refSudoku, n){
+        for(let scanX = 0; scanX<9 ; scanX++){
+            for(let scanY = 0; scanY<9 ; scanY++){
+                if(refSudoku.grid[scanY][scanX] === n){
+                    this.replaceCol(refSudoku, scanX)
+                    this.replaceRow(refSudoku, scanY)
+                    refSudoku.grid[scanY][scanX] = "#"
+                }
+            }
+        }
+    }
+
 }
 
+module.exports = ToolBox
+
+/*
 const mySudok = new Sudoku(grid)
 const copiedSudoku = copy(mySudok.grid)
 var n = 6
@@ -42,17 +62,7 @@ var centers = [[1,1],[4,1],[7,1],[1,4],[4,4],[7,4],[1,7],[4,7],[7,7]]
 
 
 
-const hideImpossibilities = function(refSudoku, n){
-    for(let scanX = 0; scanX<9 ; scanX++){
-        for(let scanY = 0; scanY<9 ; scanY++){
-            if(refSudoku.grid[scanY][scanX] === n){
-                replaceCol(refSudoku, scanX)
-                replaceRow(refSudoku, scanY)
-                refSudoku.grid[scanY][scanX] = "#"
-            }
-        }
-    }
-}
+
 
 copiedSudoku.displayGrid("initial number: "+n)
 hideImpossibilities(copiedSudoku, n)
@@ -77,3 +87,4 @@ centers.forEach(center => {
 mySudok.displayGrid("final mod")
 
 
+*/
